a4b5ac92640779e6d4e7b2e6626aa7f1b88afc87
Daisuke Yokogawa <diechan.dai.1031@gmail.com>
2020-04-05 00:10:08 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
-module.exports = {
+module.exports = ( env, argv ) => ({
   entry: './src/entry.js',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      }
+    ]
   }
-};
+});
